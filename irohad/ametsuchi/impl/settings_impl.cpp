8fc6bf9007dc33ba2c877495a4aee4c8e7d2d499
artyom-yurin <artem_yrin@mail.ru>
2019-06-24 12:12:37 +0300

    Added protection against lack of access to the database
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

--- irohad/ametsuchi/impl/settings_impl.cpp
+++ irohad/ametsuchi/impl/settings_impl.cpp
@@ -7,9 +7,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "settings_impl.hpp"
 #include <sstream>
 #include "boost/optional.hpp"
+#include "settings_impl.hpp"
 
 namespace iroha {
 
@@ -17,7 +17,8 @@ namespace iroha {
 
     SettingsImpl::SettingsImpl(
         const std::shared_ptr<iroha::ametsuchi::SettingQuery> &setting_query) {
-      max_description_size = setValueFromDB<size_t>(setting_query, "MaxDescriptionSize", 64);
+      max_description_size =
+          setValueFromDB<size_t>(setting_query, "MaxDescriptionSize", 64);
     }
 
     size_t SettingsImpl::getMaxDescriptionSize() const {
@@ -29,15 +30,19 @@ namespace iroha {
         const std::shared_ptr<iroha::ametsuchi::SettingQuery> &setting_query,
         const shared_model::interface::types::SettingKeyType &setting_key,
         T default_value) {
-      auto value = setting_query->getSettingValue(setting_key);
-      if (value) {
-        std::istringstream ss(value.get());
-        T num;
-        ss >> num;
-        if (ss.fail() or ss.tellg() != -1) {
-          return default_value;
+      if (setting_query) {
+        auto value = setting_query->getSettingValue(setting_key);
+        if (value) {
+          std::istringstream ss(value.get());
+          T num;
+          ss >> num;
+          if (ss.fail() or ss.tellg() != -1) {
+            return default_value;
+          } else {
+            return num;
+          }
         } else {
-          return num;
+          return default_value;
         }
       } else {
         return default_value;
@@ -46,17 +51,22 @@ namespace iroha {
 
     template <>
     shared_model::interface::types::SettingValueType
-    SettingsImpl::setValueFromDB<shared_model::interface::types::SettingValueType>(
+    SettingsImpl::setValueFromDB<
+        shared_model::interface::types::SettingValueType>(
         const std::shared_ptr<iroha::ametsuchi::SettingQuery> &setting_query,
         const shared_model::interface::types::SettingKeyType &setting_key,
         shared_model::interface::types::SettingValueType default_value) {
-      auto value = setting_query->getSettingValue(setting_key);
-      if (value) {
-        return value.get();
+      if (setting_query) {
+        auto value = setting_query->getSettingValue(setting_key);
+        if (value) {
+          return value.get();
+        } else {
+          return default_value;
+        }
       } else {
         return default_value;
       }
     }
 
-  } // namespace ametsuchi
-} // namespace iroha
+  }  // namespace ametsuchi
+}  // namespace iroha
